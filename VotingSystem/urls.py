12c9cc996b424d66562055b5ad6af35f8d46c3ca
Eduardo Campos <92491094+EduuuCR@users.noreply.github.com>
2025-01-13 14:35:19 -0300

    Delete VotingSystem/urls.py

--- VotingSystem/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.competition_list, name='competition_list'),
-    path('competition/<int:competition_id>/', views.competition_detail, name='competition_detail'),
-    path('competition/<int:competition_id>/add-entry/', views.add_entry, name='add_entry'),
-    path('entry/<int:entry_id>/vote/', views.vote_entry, name='vote_entry'),
-]
