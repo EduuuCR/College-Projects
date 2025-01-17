813bfcd171f6921e8f04f7ce1523c7ef59f9294b
Eduardo Campos <92491094+EduuuCR@users.noreply.github.com>
2025-01-17 09:43:34 -0300

    added main part

---
+++ QuizSystem/src/main/Main.java
@@ -0,0 +1,10 @@
+
+// Main.java
+
+// Entry point for the Quiz System application
+public class Main {
+    public static void main(String[] args) {
+        QuizSystem quizSystem = new QuizSystem();
+        quizSystem.start();
+    }
+}
